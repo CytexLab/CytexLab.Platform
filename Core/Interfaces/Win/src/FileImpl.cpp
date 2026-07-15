36428b8dd5b8242d23c2c1c148f8022b12169f74
CytexLab <cytexlab.github@gmail.com>
2026-07-15 20:32:36 +0700

    Добавлена лицензия MPL 2 для проекта

--- Core/Interfaces/Win/src/FileImpl.cpp
+++ Core/Interfaces/Win/src/FileImpl.cpp
@@ -1,3 +1,12 @@
+/* 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+ * 
+ * CytexLab (c) 2026
+ */
+
+
 #include "FileImpl.hpp"
 #include "WinImports.hpp"
 #include "Mem.hpp"
