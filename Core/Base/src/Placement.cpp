9f602f88f9b4453c23714dd72b70562dd72a0129
CytexLab <cytexlab.github@gmail.com>
2026-07-16 16:23:36 +0700

    Добавлена лицензия к исходникам модуля Base

--- Core/Base/src/Placement.cpp
+++ Core/Base/src/Placement.cpp
@@ -1,3 +1,11 @@
+/* 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+ * 
+ * CytexLab (c) 2026
+ */
+
 #include "Placement.hpp"
 
 LPVOID operator new(UINT64, LPVOID ptr) noexcept
