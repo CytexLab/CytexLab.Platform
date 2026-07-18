9b84e3648e503b35c58a1751cb6de740736e426c
CytexLab <cytexlab.github@gmail.com>
2026-07-18 19:53:07 +0700

    SystemImpl.cpp разделен на несколько файлов (Оптимизация)

---
+++ Core/Interfaces/Win/src/SystemImpl.Console.cpp
@@ -0,0 +1,275 @@
+/* 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+ * 
+ * CytexLab (c) 2026
+ */
+
+#include "SystemImpl.hpp"
+#include "WinImports.hpp"
+#include "ConsoleImpl.hpp"
+#include "FileImpl.hpp"
+#include "PipeImpl.hpp"
+#include "Placement.hpp"
+
+CytexLab::Interface::ISystemResult SystemImpl::CreateConsole(CytexLab::Interface::IConsole*& Out)
+{
+    HANDLE heap = ::GetProcessHeap();
+    LPVOID mem = ::HeapAlloc(heap, 0, sizeof(ConsoleImpl));
+
+    if (!mem)
+    {
+        UINT32 error = ::GetLastError();
+        return {
+                FALSE,
+                CytexLab::Interface::ISystemError::SystemError,
+                {
+                        TRUE,
+                        Unicode::ConvertError::None,
+                        {
+                                TRUE,
+                                Unicode::ConvertError::None,
+                                0
+                        },
+                        0,
+                        0
+                },
+                error
+        };
+    }
+
+    ConsoleImpl* ci = new (mem) ConsoleImpl();
+
+    CytexLab::Interface::IConsoleSettings s;
+    s = {FALSE, FALSE};
+
+    CytexLab::Interface::IConsoleHandle h;
+    h = {::GetStdHandle(STD_INPUT_HANDLE),
+         ::GetStdHandle(STD_OUTPUT_HANDLE)
+    };
+
+    CytexLab::Interface::IConsoleLink l;
+    l = {s, h};
+
+    ci->SetLink(l);
+
+    Out = (CytexLab::Interface::IConsole*) ci;
+
+    return {
+            TRUE,
+            CytexLab::Interface::ISystemError::None,
+            {
+                    TRUE,
+                    Unicode::ConvertError::None,
+                    {
+                            TRUE,
+                            Unicode::ConvertError::None,
+                            0
+                    },
+                    0,
+                    0
+            },
+            0
+    };
+}
+
+CytexLab::Interface::ISystemResult SystemImpl::RedirectConsole(CytexLab::Interface::IConsole* Console, CytexLab::Interface::IFile* Out, CytexLab::Interface::IFile* In)
+{
+    if (!Console)
+        return {
+                FALSE,
+                CytexLab::Interface::ISystemError::NullPointer,
+                {
+                        TRUE,
+                        Unicode::ConvertError::None,
+                        {
+                                TRUE,
+                                Unicode::ConvertError::None,
+                                0
+                        },
+                        0,
+                        0
+                },
+                0
+        };
+
+    CytexLab::Interface::IConsoleSettings s;
+    CytexLab::Interface::IConsoleHandle h;
+
+    if (Out)
+    {
+        s.hOutIsFile = TRUE;
+
+        FileImpl* fi = (FileImpl*)Out;
+        h.hOut = fi->GetHandle();
+    }
+    else
+    {
+        s.hOutIsFile = FALSE;
+        h.hOut = ::GetStdHandle(STD_OUTPUT_HANDLE);
+    }
+
+    if (In)
+    {
+        s.hInIsFile = TRUE;
+
+        FileImpl* fi = (FileImpl*)In;
+        h.hIn = fi->GetHandle();
+    }
+    else
+    {
+        s.hInIsFile = FALSE;
+        h.hIn = ::GetStdHandle(STD_INPUT_HANDLE);
+    }
+
+    CytexLab::Interface::IConsoleLink l = {s, h};
+
+    ConsoleImpl* ci = (ConsoleImpl*) Console;
+    ci->SetLink(l);
+
+    return {
+            TRUE,
+            CytexLab::Interface::ISystemError::None,
+            {
+                    TRUE,
+                    Unicode::ConvertError::None,
+                    {
+                            TRUE,
+                            Unicode::ConvertError::None,
+                            0
+                    },
+                    0,
+                    0
+            },
+            0
+    };
+}
+
+CytexLab::Interface::ISystemResult SystemImpl::RedirectConsole(CytexLab::Interface::IConsole* Console, CytexLab::Interface::IPipe* Pipe)
+{
+    if (!Console)
+        return {
+                FALSE,
+                CytexLab::Interface::ISystemError::NullPointer,
+                {
+                        TRUE,
+                        Unicode::ConvertError::None,
+                        {
+                                TRUE,
+                                Unicode::ConvertError::None,
+                                0
+                        },
+                        0,
+                        0
+                },
+                0
+        };
+
+    CytexLab::Interface::IConsoleSettings s;
+    CytexLab::Interface::IConsoleHandle h;
+
+    if (!Pipe)
+    {
+        s.hOutIsFile = FALSE;
+        s.hInIsFile = FALSE;
+
+        h.hIn = ::GetStdHandle(STD_INPUT_HANDLE);
+        h.hOut = ::GetStdHandle(STD_OUTPUT_HANDLE);
+    }
+    else
+    {
+        PipeImpl* pi = (PipeImpl*) Pipe;
+
+        s.hOutIsFile = TRUE;
+        s.hInIsFile = TRUE;
+
+        h.hIn = pi->GetHandle();
+        h.hOut = pi->GetHandle();
+    }
+
+    CytexLab::Interface::IConsoleLink l = {s, h};
+
+    ConsoleImpl* ci = (ConsoleImpl*) Console;
+    ci->SetLink(l);
+
+    return {
+            TRUE,
+            CytexLab::Interface::ISystemError::None,
+            {
+                    TRUE,
+                    Unicode::ConvertError::None,
+                    {
+                            TRUE,
+                            Unicode::ConvertError::None,
+                            0
+                    },
+                    0,
+                    0
+            },
+            0
+    };
+}
+
+CytexLab::Interface::ISystemResult SystemImpl::DestroyConsole(CytexLab::Interface::IConsole* Console)
+{
+    if (!Console)
+        return {
+                FALSE,
+                CytexLab::Interface::ISystemError::NullPointer,
+                {
+                        TRUE,
+                        Unicode::ConvertError::None,
+                        {
+                                TRUE,
+                                Unicode::ConvertError::None,
+                                0
+                        },
+                        0,
+                        0
+                },
+                0
+        };
+
+    ConsoleImpl* ci = (ConsoleImpl*) Console;
+    HANDLE heap = ::GetProcessHeap();
+    BOOL result = ::HeapFree(heap, 0, ci);
+
+    if (!result)
+    {
+        UINT32 error = ::GetLastError();
+        return {
+                FALSE,
+                CytexLab::Interface::ISystemError::SystemError,
+                {
+                        TRUE,
+                        Unicode::ConvertError::None,
+                        {
+                                TRUE,
+                                Unicode::ConvertError::None,
+                                0
+                        },
+                        0,
+                        0
+                },
+                error
+        };
+    }
+
+    return {
+            TRUE,
+            CytexLab::Interface::ISystemError::None,
+            {
+                    TRUE,
+                    Unicode::ConvertError::None,
+                    {
+                            TRUE,
+                            Unicode::ConvertError::None,
+                            0
+                    },
+                    0,
+                    0
+            },
+            0
+    };
+}
