cf6ef19ae0a1549f49a783f8fc2b77a6235e873a
CytexLab <cytexlab.github@gmail.com>
2026-07-19 18:49:30 +0700

    Базовая реализация процессов готова

--- Test/TestRunner/src/main.cpp
+++ Test/TestRunner/src/main.cpp
@@ -14,25 +14,25 @@ using namespace CytexLab::Interface;
 extern "C" void startup()
 {
     ISystem* system = Fabric::SystemFabric::Create();
+
     IConsole* console;
 	system->CreateConsole(console);
 
-    UINT64 args;
-    ISystemResult result = system->GetArgs(&args);
+    IPipe* pipe;
+    system->CreatePipe(pipe, U"MyTestPipe");
 
-    ECHAR str1[256];
-    ECHAR str2[256];
-    ECHAR str3[256];
+    IProcess* process;
+    system->CreateProcess(process, U"F:\\CytexLab.Platform\\Dist\\Windows.Debug\\SubProcess.exe");
 
-    LPECHAR buf[3] = {str1, str2, str3};
+    pipe->Connect();
 
-    ISystemResult result2 = system->GetArgv(buf);
+    ECHAR buf[128];
+    pipe->Read(buf, 128, NULLPTR);
 
-    for (UINT64 i = 0; i < args; i++)
-    {
-        console->WriteLine(&buf[i][0]);
-    }
+    console->WriteLine(buf);
 
+    system->DestroyProcess(process);
+    system->DestroyPipe(pipe);
     system->DestroyConsole(console);
     system->ExitProcess(0);
 }
