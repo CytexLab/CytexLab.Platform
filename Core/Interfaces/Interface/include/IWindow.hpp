3b7eb405683bbf012758a727592c7676fc9c5195
CytexLab <cytexlab.github@gmail.com>
2026-07-21 15:38:54 +0700

    Добавлен интерфейс для окна

---
+++ Core/Interfaces/Interface/include/IWindow.hpp
@@ -0,0 +1,24 @@
+/* 
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. 
+ * 
+ * CytexLab (c) 2026
+ */
+
+#pragma once
+
+#include "Types.hpp"
+
+namespace CytexLab
+{
+    namespace Interface
+    {
+        class IWindow
+        {
+        public:
+            virtual void Close() = 0;
+            virtual void Process() = 0;
+        };
+    }
+}
