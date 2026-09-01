72cc932e5e3cdc96c52d76dea3c6cd3c7eb5344f
CytexLab <cytexlab.github@gmail.com>
2026-09-01 17:12:32 +0700

    Дописана реализация копирования данных через SSE4.2 и AVX

--- Test/Dev/src/mem.asm
+++ Test/Dev/src/mem.asm
@@ -1,34 +1,56 @@
 .section .text
 
 .global memcpy_sse42_asm
+.global memcpy_avx_asm
 
 memcpy_sse42_asm:
     cmp r8, 0
-    jz .done
+    jz .memcpy_done
 
     test rcx, 15
-    jnz .loop_anling
+    jnz .memcpy_sse42_loop_unaligned
     test rdx, 15
-    jnz .loop_anling
-.loop_ling:
+    jnz .memcpy_sse42_loop_unaligned
+.memcpy_sse42_loop_aligned:
     movaps xmm0, [rcx]
     movaps [rdx], xmm0
 
     add rcx, 16
     add rdx, 16
     sub r8, 16
-    jz .done
-
-    jmp .loop_ling
-.loop_anling:
+    jz .memcpy_done
+.memcpy_sse42_loop_unaligned:
     movups xmm0, [rcx]
     movups [rdx], xmm0
 
     add rcx, 16
     add rdx, 16
     sub r8, 16
-    jz .done
-
-    jmp .loop_anling
-.done:
+    jz .memcpy_done
+.memcpy_done:
     ret
+
+memcpy_avx_asm:
+    cmp r8, 0
+    jz .memcpy_done
+
+    test rcx, 31
+    jnz .memcpy_avx_loop_unaligned
+    test rdx, 31
+    jnz .memcpy_avx_loop_unaligned
+.memcpy_avx_loop_aligned:
+    vmovdqa ymm0, [rcx]
+    vmovdqa [rdx], ymm0
+
+    add rcx, 32
+    add rdx, 32
+    sub r8, 32
+    jz .memcpy_done
+.memcpy_avx_loop_unaligned:
+    vmovdqu ymm0, [rcx]
+    vmovdqu [rdx], ymm0
+
+    add rcx, 32
+    add rdx, 32
+    sub r8, 32
+    jz .memcpy_done
