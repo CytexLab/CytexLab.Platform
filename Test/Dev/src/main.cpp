aef8d89903d559d93c3998c2291f835afa793845
CytexLab <cytexlab.github@gmail.com>
2026-09-01 16:43:46 +0700

    Начинаем тестировать ядро

--- Test/Dev/src/main.cpp
+++ Test/Dev/src/main.cpp
@@ -9,20 +9,32 @@
  * Для получения коммерческой лицензии: programminyka@mail.ru
  */
 
-#include "Fabric.hpp"
-#include "Panic.hpp"
+#include "Mem.hpp"
+#include "Platform.hpp"
+
+typedef void (*fail_callback_sign)(UINT64 code);
+typedef void (*memcpy_sse42_asm_func)(LPCVOID From, LPVOID To, UINT64 Count);
+typedef void (*memcpy_avx_asm_func)(LPCVOID From, LPVOID To, UINT64 Count);
+
+CYTEXLAB_API void proc_support(BOOL sse42, BOOL avx);
+CYTEXLAB_API void set_fail_callback(fail_callback_sign callback);
+CYTEXLAB_API void memcpy_sse42_set(memcpy_sse42_asm_func func);
+CYTEXLAB_API void memcpy_avx_set(memcpy_avx_asm_func func);
+
+extern "C" void memcpy_sse42_asm(LPCVOID From, LPVOID To, UINT64 Count);
+
+void callback(UINT64 code)
+{
+}
 
 extern "C" void startup()
 {
-  cl::SystemPlatform::Panic::Init();
+  proc_support(FALSE, FALSE);
+  set_fail_callback(callback);
+  memcpy_sse42_set(memcpy_sse42_asm);
 
-  STACK_FRAME();
+  UINT8 buf1[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF};
+  UINT8 buf2[32];
 
-  cl::Interface::ISystem* system = cl::SystemPlatform::Fabric::Create();
-
-  PANIC(U"Test");
-
-  cl::SystemPlatform::Panic::StackPop();
-
-  system->Exit(0);
+  memcpy_sse42(buf1, buf2, 16);
 }
