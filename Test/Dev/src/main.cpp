aba65ebc0950906503eea51018f71b19943f6b2b
CytexLab <cytexlab.github@gmail.com>
2026-09-01 17:12:32 +0700

    Дописана реализация копирования данных через SSE4.2 и AVX

--- Test/Dev/src/main.cpp
+++ Test/Dev/src/main.cpp
@@ -22,6 +22,7 @@ CYTEXLAB_API void memcpy_sse42_set(memcpy_sse42_asm_func func);
 CYTEXLAB_API void memcpy_avx_set(memcpy_avx_asm_func func);
 
 extern "C" void memcpy_sse42_asm(LPCVOID From, LPVOID To, UINT64 Count);
+extern "C" void memcpy_avx_asm(LPCVOID From, LPVOID To, UINT64 Count);
 
 void callback(UINT64 code)
 {
@@ -29,12 +30,14 @@ void callback(UINT64 code)
 
 extern "C" void startup()
 {
-  proc_support(FALSE, FALSE);
+  proc_support(TRUE, TRUE);
   set_fail_callback(callback);
   memcpy_sse42_set(memcpy_sse42_asm);
+  memcpy_avx_set(memcpy_avx_asm);
 
-  UINT8 buf1[16] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF};
+  UINT8 buf1[32] = {0x0,  0x1,  0x2,  0x3,  0x4,  0x5,  0x6,  0x7,  0x8,  0x9,  0xA,  0xB,  0xC,  0xD,  0xE,  0xF,
+                    0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17, 0x18, 0x19, 0x1A, 0x1B, 0x1C, 0x1D, 0x1E, 0x1F};
   UINT8 buf2[32];
 
-  memcpy_sse42(buf1, buf2, 16);
+  memcpy(buf1, buf2, 32);
 }
