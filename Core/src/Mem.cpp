f1fd8b5d1d7a331020f7264a091d56caa4548302
CytexLab <cytexlab.github@gmail.com>
2026-09-02 16:26:16 +0700

    Добавлена поддержка memset функции

--- Core/src/Mem.cpp
+++ Core/src/Mem.cpp
@@ -14,10 +14,14 @@
 
 typedef void (*memcpy_sse42_asm_func)(LPCVOID From, LPVOID To, UINT64 Count);
 typedef void (*memcpy_avx_asm_func)(LPCVOID From, LPVOID To, UINT64 Count);
+typedef void (*memset_sse42_asm_func)(LPVOID Mem, UINT8 Byte, UINT64 Count);
+typedef void (*memset_avx_asm_func)(LPVOID Mem, UINT8 Byte, UINT64 Count);
 typedef void (*fail_callback_sign)(UINT64 code);
 
 memcpy_sse42_asm_func memcpy_sse42_asm = nullptr;
 memcpy_avx_asm_func memcpy_avx_asm = nullptr;
+memset_sse42_asm_func memset_sse42_asm = nullptr;
+memset_avx_asm_func memset_avx_asm = nullptr;
 
 extern BOOL is_proc_support_sse42();
 extern BOOL is_proc_support_avx();
@@ -42,6 +46,16 @@ CYTEXLAB_API void memcpy_sse42_set(memcpy_sse42_asm_func func)
 CYTEXLAB_API void memcpy_avx_set(memcpy_avx_asm_func func)
 {
   memcpy_avx_asm = func;
+}
+
+CYTEXLAB_API void memset_sse42_set(memset_sse42_asm_func func)
+{
+  memset_sse42_asm = func;
+}
+
+CYTEXLAB_API void memset_avx_set(memset_avx_asm_func func)
+{
+  memset_avx_asm = func;
 }
 
 CYTEXLAB_API void memcpy_sse42(LPCVOID From, LPVOID To, UINT64 Count)
@@ -94,5 +108,59 @@ CYTEXLAB_API void memcpy(LPCVOID From, LPVOID To, UINT64 Count)
         :
         : "S"(From), "D"(To), "c"(Count)
         : "memory");
+  }
+}
+
+CYTEXLAB_API void memset_sse42(LPVOID To, UINT8 Byte, UINT64 Count)
+{
+  if (!updated_info)
+    update_info();
+
+  if (Count % 16 == 0)
+    get_fail_callback()(2);
+
+  if (support_sse42)
+    memset_sse42_asm(To, Byte, Count);
+  else
+    get_fail_callback()(1);
+}
+
+CYTEXLAB_API void memset_avx(LPVOID To, UINT8 Byte, UINT64 Count)
+{
+  if (!updated_info)
+    update_info();
+
+  if (Count % 32 == 0)
+    get_fail_callback()(4);
+
+  if (support_avx)
+    memset_avx_asm(To, Byte, Count);
+  else
+    get_fail_callback()(3);
+}
+
+CYTEXLAB_API void memset(LPVOID To, UINT8 Byte, UINT64 Count)
+{
+  if (!updated_info)
+    update_info();
+
+  if (support_avx && Count >= 32 && (Count % 32) == 0)
+    memset_avx(To, Byte, Count);
+  else if (support_sse42 && Count >= 16 && (Count % 16) == 0)
+    memset_sse42(To, Byte, Count);
+  else
+  {
+    asm volatile (
+      ".loop:\n"
+      "mov [%0], %1\n"
+      "add %0, 1\n"
+      "sub %2, 1\n"
+      "jz .done\n"
+      "jmp .loop\n"
+      ".done:\n"
+      :
+      : "r" (+To), "r" (Byte), "r" (+Count)
+      : "memory", "cc"
+    );
   }
 }
