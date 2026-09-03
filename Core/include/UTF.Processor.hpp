61fdc2e076de5e2c09808b731123205497163f0b
CytexLab <cytexlab.github@gmail.com>
2026-09-03 19:54:16 +0700

    Добавлено объявления процессор для отдельных символов

--- Core/include/UTF.Processor.hpp
+++ Core/include/UTF.Processor.hpp
@@ -9,5 +9,40 @@
  * Для получения коммерческой лицензии: programminyka@mail.ru
  */
 
-
 #pragma once
+
+#include "Platform.hpp"
+
+namespace cl
+{
+  namespace UTF
+  {
+    class Processor
+    {
+    public:
+      class SymbolProcessor
+      {
+      public:
+        enum class Error : UINT8
+        {
+          None = 0,
+          NullPointer = 1,
+          InvalidByte = 2
+        };
+
+        struct Result
+        {
+          BOOL Success;
+          Error Error;
+          UINT8 ReadedInput;
+          UINT8 WrittenOutput;
+        };
+
+        CYTEXLAB_API static Result ConvertU8oU32(LPCCHAR U8_Char, LPECHAR U32_Char);
+        CYTEXLAB_API static Result ConvertU16oU32(LPCWCHAR U16_Char, LPECHAR U32_Char);
+        CYTEXLAB_API static Result ConvertU32oU8(LPCECHAR U32_Char, LPCHAR U8_Char);
+        CYTEXLAB_API static Result ConvertU32oU16(LPCECHAR U32_Char, LPWCHAR U16_Char);
+      };
+    };
+  }
+}
