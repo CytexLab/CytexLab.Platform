081d6e58bd854a64957dc51d168d6fc4a9d4f723
CytexLab <cytexlab.github@gmail.com>
2026-09-05 13:40:20 +0700

    Добавлена поддержка конвертации UTF-16 -> UTF-32

--- Core/Core.UTF/%.Processor/%.Processor.Symbols/src/UTF.Processor.cpp
+++ Core/Core.UTF/%.Processor/%.Processor.Symbols/src/UTF.Processor.cpp
@@ -28,7 +28,8 @@ CYTEXLAB_API cl::UTF::Processor::SymbolProcessor::Result cl::UTF::Processor::Sym
   {
     ECHAR out = one;
     *U32_Char = out;
-    result.ReadedInput = 1; result.WrittenOutput = 1;
+    result.ReadedInput = 1;
+    result.WrittenOutput = 1;
     return result;
   }
   else if ((one & 0xE0) == 0xC0)
@@ -42,7 +43,8 @@ CYTEXLAB_API cl::UTF::Processor::SymbolProcessor::Result cl::UTF::Processor::Sym
 
     ECHAR out = (((ECHAR)(one & 0x1F)) << 6) | ((ECHAR)(two & 0x3F));
     *U32_Char = out;
-    result.ReadedInput = 2; result.WrittenOutput = 1;
+    result.ReadedInput = 2;
+    result.WrittenOutput = 1;
     return result;
   }
   else if ((one & 0xF0) == 0xE0)
@@ -57,7 +59,8 @@ CYTEXLAB_API cl::UTF::Processor::SymbolProcessor::Result cl::UTF::Processor::Sym
 
     ECHAR out = (((ECHAR)(one & 0x0F)) << 12) | (((ECHAR)(two & 0x3F)) << 6) | ((ECHAR)(three & 0x3F));
     *U32_Char = out;
-    result.ReadedInput = 3; result.WrittenOutput = 1;
+    result.ReadedInput = 3;
+    result.WrittenOutput = 1;
     return result;
   }
   else if ((one & 0xF8) == 0xF0)
@@ -73,7 +76,49 @@ CYTEXLAB_API cl::UTF::Processor::SymbolProcessor::Result cl::UTF::Processor::Sym
 
     ECHAR out = (((ECHAR)(one & 0x07)) << 18) | (((ECHAR)(two & 0x3F)) << 12) | (((ECHAR)(three & 0x3F)) << 6) | ((ECHAR)(four & 0x3F));
     *U32_Char = out;
-    result.ReadedInput = 4; result.WrittenOutput = 1;
+    result.ReadedInput = 4;
+    result.WrittenOutput = 1;
+    return result;
+  }
+  else
+  {
+    result = {FALSE, Error::InvalidByte, 0, 0};
+    return result;
+  }
+}
+
+CYTEXLAB_API cl::UTF::Processor::SymbolProcessor::Result cl::UTF::Processor::SymbolProcessor::ConvertU16oU32(LPCWCHAR U16_Char, LPECHAR U32_Char)
+{
+  Result result = {TRUE, Error::None, 0, 0};
+
+  if (!U16_Char || !U32_Char)
+  {
+    result = {FALSE, Error::NullPointer, 0, 0};
+    return result;
+  }
+
+  WCHAR one = *U16_Char++;
+
+  if (one <= 0xD7FF || (one >= 0xE000 && one <= 0xFFFF))
+  {
+    ECHAR out = one;
+    *U32_Char = out;
+    result.ReadedInput = 1; result.WrittenOutput = 1;
+    return result;
+  }
+  else if ((one & 0xFC00) == 0xD800)
+  {
+    WCHAR two = *U16_Char++;
+    if ((two & 0xFC00) != 0xDC00)
+    {
+      result = {FALSE, Error::InvalidContinueByte, 0, 0};
+      return result;
+    }
+
+    ECHAR out = ((ECHAR)(one & 0x3FF) << 10 | (ECHAR)(two & 0x3FF)) + 0x10000;
+    *U32_Char = out;
+
+    result.ReadedInput = 2; result.WrittenOutput = 1;
     return result;
   }
   else
