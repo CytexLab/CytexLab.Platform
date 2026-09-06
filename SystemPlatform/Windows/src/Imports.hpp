91c66335dbbebe80af514713b2bac13284bd88a5
CytexLab <cytexlab.github@gmail.com>
2026-09-06 15:48:57 +0700

    Добавлен системный пул

--- SystemPlatform/Windows/src/Imports.hpp
+++ SystemPlatform/Windows/src/Imports.hpp
@@ -16,4 +16,25 @@
 extern "C"
 {
   __declspec(dllimport) void RtlExitUserProcess(UINT32 ExitStatus);
+  __declspec(dllimport) UINT64 NtAllocateVirtualMemory(LPVOID ProcessHandle, LPVOID* BaseAddress, UINT64 ZeroBits, UINT64 RegionSize, UINT32 AllocationType, UINT32 Protect);
+  __declspec(dllimport) UINT32 NtFreeVirtualMemory(LPVOID ProcessHandle, LPVOID* BaseAddress, UINT64 RegionSize, UINT32 FreeType);
 }
+
+#define PAGE_NOACCESS          0x01
+#define PAGE_READONLY          0x02
+#define PAGE_READWRITE         0x04
+#define PAGE_WRITECOPY         0x08
+#define PAGE_EXECUTE           0x10
+#define PAGE_EXECUTE_READ      0x20
+#define PAGE_EXECUTE_READWRITE 0x40
+#define PAGE_EXECUTE_WRITECOPY 0x80
+#define PAGE_GUARD             0x100
+#define PAGE_NOCACHE           0x200
+#define PAGE_WRITECOMBINE      0x400
+#define MEM_COMMIT             0x00001000
+#define MEM_RESERVE            0x00002000
+#define MEM_RESET              0x00080000
+#define MEM_RELEASE            0x00008000
+#define MEM_DECOMMIT           0x00004000
+#define STATUS_SUCCESS         ((UINT32)0x00000000L)
+#define NtCurrentProcess()     (LPVOID)-1
