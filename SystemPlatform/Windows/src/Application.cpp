21f8b14a3ca809e7850c05982bada6d05b6d7568
CytexLab <cytexlab.github@gmail.com>
2026-09-06 14:14:28 +0700

    Начинаем писать SystemPlatform под Windows

---
+++ SystemPlatform/Windows/src/Application.cpp
@@ -0,0 +1,10 @@
+/*
+ * CytexLab Platform
+ * Copyright (c) 2026 CytexLab
+ *
+ * Данный файл является частью платформы CytexLab и распространяется
+ * согласно условиям CytexLab Public Community License (CL2P) v1.0.
+ * Полный текст лицензии доступен в файле LICENSE.txt.
+ *
+ * Для получения коммерческой лицензии: programminyka@mail.ru
+ */
