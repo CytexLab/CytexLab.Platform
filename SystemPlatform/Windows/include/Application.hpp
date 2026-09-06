1e4f7b87a71b2d6700445a02026921d606553dd2
CytexLab <cytexlab.github@gmail.com>
2026-09-06 14:36:09 +0700

    Добавлена возможность создавать классы приложения

--- SystemPlatform/Windows/include/Application.hpp
+++ SystemPlatform/Windows/include/Application.hpp
@@ -13,14 +13,40 @@
 
 #include "Platform.hpp"
 
+#ifdef CYTEXLAB_SYSTEMPLATFORM_WINDOWS_API_EXPORT
+#define CYTEXLAB_SYSTEMPLATFORM_WINDOWS_API __declspec(dllexport)
+#else
+#define CYTEXLAB_SYSTEMPLATFORM_WINDOWS_API __declspec(dllimport)
+#endif
+
 namespace cl
 {
   namespace SystemPlatform
   {
     class Application
     {
+    private:
+      BOOL inited = FALSE;
+
     public:
-      static void Exit(UINT32 Code);
+      CYTEXLAB_SYSTEMPLATFORM_WINDOWS_API void Init();
+      CYTEXLAB_SYSTEMPLATFORM_WINDOWS_API void Exit(UINT32 Code);
+    };
+
+    class UserApplication
+    {
+    public:
+      virtual void Run(Application& App) = 0;
+      virtual ~UserApplication() = 0;
     };
   }
 }
+
+#define REGISTER_USER_APP(UserAppClass) \
+  extern "C" void clSysStartup() \
+  { \
+    cl::SystemPlatform::Application app; \
+    app.Init(); \
+    UserAppClass user_app; \
+    user_app.Run(app); \
+  }
