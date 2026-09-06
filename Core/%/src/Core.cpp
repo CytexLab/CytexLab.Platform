0a16ea1010e78a63ebb799852dcf3b2963f897a9
CytexLab <cytexlab.github@gmail.com>
2026-09-06 12:04:53 +0700

    Измененная система экспортов

--- Core/%/src/Core.cpp
+++ Core/%/src/Core.cpp
@@ -9,7 +9,8 @@
  * Для получения коммерческой лицензии: programminyka@mail.ru
  */
 
-#define CYTEXLAB_PLATFORM_EXPORT
+#define CYTEXLAB_CORE_API __declspec(dllexport)
+
 #include "Platform.hpp"
 
 typedef void (*fail_callback_sign)(UINT64 code);
@@ -19,28 +20,28 @@ static BOOL support_avx = FALSE;
 
 fail_callback_sign fail_callback;
 
-CYTEXLAB_API void proc_support(BOOL sse42, BOOL avx)
+CYTEXLAB_CORE_API void proc_support(BOOL sse42, BOOL avx)
 {
   support_sse42 = sse42;
   support_avx = avx;
 }
 
-CYTEXLAB_API void set_fail_callback(fail_callback_sign callback)
+CYTEXLAB_CORE_API void set_fail_callback(fail_callback_sign callback)
 {
   fail_callback = callback;
 }
 
-CYTEXLAB_API fail_callback_sign get_fail_callback()
+CYTEXLAB_CORE_API fail_callback_sign get_fail_callback()
 {
   return fail_callback;
 }
 
-CYTEXLAB_API BOOL is_proc_support_sse42()
+CYTEXLAB_CORE_API BOOL is_proc_support_sse42()
 {
   return support_sse42;
 }
 
-CYTEXLAB_API BOOL is_proc_support_avx()
+CYTEXLAB_CORE_API BOOL is_proc_support_avx()
 {
   return support_avx;
 }
