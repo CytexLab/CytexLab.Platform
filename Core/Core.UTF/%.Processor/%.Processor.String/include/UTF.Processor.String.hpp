8d9ef790b226c3fe174173e1c8538500a9122574
CytexLab <cytexlab.github@gmail.com>
2026-09-06 12:18:43 +0700

    Добавлен процессор UTF для целых строк

---
+++ Core/Core.UTF/%.Processor/%.Processor.String/include/UTF.Processor.String.hpp
@@ -0,0 +1,70 @@
+/*
+ * CytexLab Platform
+ * Copyright (c) 2026 CytexLab
+ *
+ * Данный файл является частью платформы CytexLab и распространяется
+ * согласно условиям CytexLab Public Community License (CL2P) v1.0.
+ * Полный текст лицензии доступен в файле LICENSE.txt.
+ *
+ * Для получения коммерческой лицензии: programminyka@mail.ru
+ */
+
+#pragma once
+
+#include "Platform.hpp"
+#include "UTF.Processor.Symbols.hpp"
+
+#ifdef CYTEXLAB_CORE_UTF_PROCESSOR_STRING_API_EXPORT
+#define CYTEXLAB_CORE_UTF_PROCESSOR_STRING_API __declspec(dllexport)
+#else
+#define CYTEXLAB_CORE_UTF_PROCESSOR_STRING_API __declspec(dllimport)
+#endif
+
+namespace cl
+{
+  namespace UTF
+  {
+    namespace Processor
+    {
+      class StringProcessor
+      {
+      public:
+        enum class Error : UINT8
+        {
+          None = 0,
+          NullPointer = 1,
+          FailConvertSymbol = 2,
+        };
+
+        struct InputReaded
+        {
+          UINT64 SymbolsReaded;
+          UINT64 BytesReaded;
+        };
+
+        struct OutputWritten
+        {
+          UINT64 SymbolsWritten;
+          UINT64 BytesWritten;
+        };
+
+        struct Metrics
+        {
+          InputReaded Input;
+          OutputWritten Output;
+        };
+
+        struct Result
+        {
+          BOOL Success;
+          Error Error;
+          SymbolProcessor::Result LastSymbol;
+          Metrics Metrics;
+        };
+
+        CYTEXLAB_CORE_UTF_PROCESSOR_STRING_API static Result GenerateEmptyResult();
+        CYTEXLAB_CORE_UTF_PROCESSOR_STRING_API static Result ConvertUTF8oUTF32(LPCCHAR U8_String, LPECHAR U32_String);
+      };
+    }
+  }
+}
