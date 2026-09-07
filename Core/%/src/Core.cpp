41fb3b6b1e71a9499ca3d8416923b2a126b44777
CytexLab <cytexlab.github@gmail.com>
2026-09-07 18:18:22 +0700

    Расширяем ядро

--- Core/%/src/Core.cpp
+++ Core/%/src/Core.cpp
@@ -14,11 +14,15 @@
 #include "Platform.hpp"
 
 typedef void (*fail_callback_sign)(UINT64 code);
+typedef void (*stack_push_sign)(LPCCHAR Module, LPCCHAR File, LPCCHAR Function, UINT64 Line);
+typedef void (*stack_pop_sign)();
 
 static BOOL support_sse42 = FALSE;
 static BOOL support_avx = FALSE;
 
 fail_callback_sign fail_callback;
+stack_push_sign stack_push;
+stack_pop_sign stack_pop;
 
 CYTEXLAB_CORE_API void proc_support(BOOL sse42, BOOL avx)
 {
@@ -34,6 +38,26 @@ CYTEXLAB_CORE_API void set_fail_callback(fail_callback_sign callback)
 CYTEXLAB_CORE_API fail_callback_sign get_fail_callback()
 {
   return fail_callback;
+}
+
+CYTEXLAB_CORE_API void set_stack_push_func(stack_push_sign func)
+{
+  stack_push = func;
+}
+
+CYTEXLAB_CORE_API void set_stack_pop_func(stack_pop_sign func)
+{
+  stack_pop = func;
+}
+
+CYTEXLAB_CORE_API stack_push_sign get_stack_push_func()
+{
+  return stack_push;
+}
+
+CYTEXLAB_CORE_API stack_pop_sign get_stack_pop_sign()
+{
+  return stack_pop;
 }
 
 CYTEXLAB_CORE_API BOOL is_proc_support_sse42()
