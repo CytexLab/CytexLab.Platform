be0455ef1233499b61e9e7cef27d1a6774196ba3
CytexLab <cytexlab.github@gmail.com>
2026-09-08 21:29:44 +0700

    Пишем преобразователь между строками и числами

--- Core/Core.UTF/%.Utils/src/StringIntConvert.cpp
+++ Core/Core.UTF/%.Utils/src/StringIntConvert.cpp
@@ -9,5 +9,40 @@
  * Для получения коммерческой лицензии: programminyka@mail.ru
  */
 
-#include "UConvert.hpp"
+#define CYTEXLAB_CORE_UTF_UTILS_STRINGINTCONVERT_API_EXPORT
+#include "StringIntConvert.hpp"
 
+CYTEXLAB_CORE_UTF_UTILS_STRINGINTCONVERT_API cl::UTF::Utils::StringIntConvert::Result cl::UTF::Utils::StringIntConvert::ConvertStringToInt(LPCECHAR String, LPUINT64 Int)
+{
+  Result result = {TRUE, Error::None};
+
+  if (String == nullptr || Int == nullptr)
+  {
+    result = {FALSE, Error::NullPointer};
+    return result;
+  }
+
+  if (*String == U'-')
+  {
+    result = {FALSE, Error::InvalidNumber};
+    return result;
+  }
+
+  if (*String == U'0')
+  {
+    *Int = 0;
+    return result;
+  }
+
+  UINT64 number = 0;
+
+  while (*String)
+  {
+    UINT64 n = *String - U'0';
+    number *= 10;
+    number += n;
+  }
+
+  *Int = number;
+  return result;
+}
