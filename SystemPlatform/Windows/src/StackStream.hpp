eb9e27e5e35fec145b6101b166075318b5041a4a
CytexLab <cytexlab.github@gmail.com>
2026-09-08 20:38:21 +0700

    Продолжаем писать

---
+++ SystemPlatform/Windows/src/StackStream.hpp
@@ -0,0 +1,30 @@
+/*
+ * CytexLab Platform
+ * Copyright (c) 2026 CytexLab
+ *
+ * Данный файл является частью платформы CytexLab и распространяется
+ * согласно условиям CytexLab Public Community License (CL2P) v1.0.
+ * Полный текст лицензии доступен в файле LICENSE.txt.
+ *
+ * Для получения коммерческой лицензии: programminyka@mail.ru
+ */
+
+#pragma once
+
+#include "StringStream.hpp"
+#include "Stack.hpp"
+
+class StackStream : public cl::SystemPlatform::StringStream
+{
+private:
+  StackItem* stack = nullptr;
+  UINT64 count = 0;
+
+public:
+  explicit StackStream(StackItem* Stack, UINT64 Count);
+
+  Stream& operator<<(LPCUINT8) override;
+  Stream& operator>>(LPUINT8) override;
+  StringStream& operator<<(LPCECHAR) override;
+  StringStream& operator>>(LPECHAR) override;
+};
