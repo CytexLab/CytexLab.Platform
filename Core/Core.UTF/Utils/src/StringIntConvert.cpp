d0b84f604dbb9c8241e662b2fdfb0c738973bfe5
CytexLab <cytexlab.github@gmail.com>
2026-09-13 17:24:42 +0700

    Применили форматирование

--- Core/Core.UTF/Utils/src/StringIntConvert.cpp
+++ Core/Core.UTF/Utils/src/StringIntConvert.cpp
@@ -107,7 +107,7 @@ CYTEXLAB_CORE_UTF_UTILS_STRINGINTCONVERT_API cl::UTF::Utils::StringIntConvert::R
       result = {FALSE, Error::InvalidNumber};
       return result;
     }
-    
+
     if (isNegative == FALSE)
     {
       if (number > INT64_MAX_DIV10 || (number == INT64_MAX_DIV10 && n > INT64_MAX_MOD10))
@@ -158,7 +158,7 @@ CYTEXLAB_CORE_UTF_UTILS_STRINGINTCONVERT_API cl::UTF::Utils::StringIntConvert::R
   ECHAR buf[21];
   UINT8 count = 0;
   UINT64 number = *Int;
-  
+
   if (number == 0)
   {
     *String++ = U'0';
@@ -186,49 +186,49 @@ CYTEXLAB_CORE_UTF_UTILS_STRINGINTCONVERT_API cl::UTF::Utils::StringIntConvert::R
 CYTEXLAB_CORE_UTF_UTILS_STRINGINTCONVERT_API cl::UTF::Utils::StringIntConvert::Result cl::UTF::Utils::StringIntConvert::ConvertIntToString(LPCINT64 Int, LPECHAR String)
 {
   Result result = {TRUE, Error::None};
-  
+
   if (!Int || !String)
   {
     result = {FALSE, Error::NullPointer};
     return result;
   }
-  
+
   INT64 tmp = *Int;
   UINT64 number = 0;
-  
+
   if (tmp > 0)
     number = (UINT64)tmp;
   else
     number = (UINT64)-tmp;
-  
+
   ECHAR buf[21];
   UINT8 count = 0;
-  
+
   if (number == 0)
   {
     *String++ = U'0';
     *String = U'\0';
     return result;
   }
-  
+
   BOOL isNegative = FALSE;
   if (*Int < 0)
     isNegative = TRUE;
-  
+
   while (number > 0)
   {
     UINT64 n = number % 10;
     buf[count++] = U'0' + n;
     number /= 10;
   }
-  
+
   if (isNegative)
     *String++ = U'-';
-  
+
   for (UINT8 i = count; i > 0; i--)
     *String++ = buf[i - 1];
-  
+
   *String = U'\0';
-  
+
   return result;
 }
